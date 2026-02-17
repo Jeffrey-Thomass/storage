ba47ba45b992288c0c07e4e16fc03f4148f90242
Jeffrey Thomas <jeffreythomas10566x@gmail.com>
2026-02-17 10:21:06 +0530

    Trying to Fix Cookie Expiry Issue

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -5,7 +5,16 @@ export default async function checkAuth(req, res, next) {
   if (!uid) {
     return res.status(401).json({ error: "Not logged!" });
   }
-  const user = await User.findOne({ _id: uid }).lean();
+  const expiryTimeInSeconds = parseInt(uid.substr(24,32), 16);
+  const currentTimeInSeconds = Math.round(Date.now() / 1000);
+  console.log(expiryTimeInSeconds, currentTimeInSeconds);
+  console.log(expiryTimeInSeconds - currentTimeInSeconds);
+
+  if(currentTimeInSeconds > expiryTimeInSeconds) {
+    res.clearCookie("uid");
+    return res.status(401).json({ error: "Not logged!" });
+  }
+  const user = await User.findOne({ _id: uid.substr(0, 24) }).lean();
   if (!user) {
     return res.status(401).json({ error: "Not logged!" });
   }
